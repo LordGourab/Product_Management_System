16b68b99c4d6cd283678a543a26eff1eda8efdf2
Lord <gourabray007@gmail.com>
2025-12-16 23:40:59 +0530

    Update CORS configuration for frontend integration

--- backend/product_management_backend/src/main/java/com/example/product_management_backend/controller/ProductController.java
+++ backend/product_management_backend/src/main/java/com/example/product_management_backend/controller/ProductController.java
@@ -6,17 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @Controller
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping("/save")
+    @PostMapping("/saveProduct")
     public ResponseEntity saveProduct(@RequestBody Product product)
     {
             return new ResponseEntity<>(productService.saveProduct(product), HttpStatus.CREATED);
@@ -31,14 +29,14 @@ public class ProductController {
     {
         return new ResponseEntity(productService.getProductById(id),HttpStatus.OK);
     }
-    @GetMapping("/delete/{id}")
+    @GetMapping("/deleteProduct/{id}")
     public ResponseEntity deleteProduct(@PathVariable Integer id)
     {
 
 
         return new ResponseEntity(productService.deleteProduct(id),HttpStatus.OK);
     }
-    @PostMapping("/update/{id}")
+    @PostMapping("/editProduct/{id}")
     public ResponseEntity editProduct(@RequestBody Product product,@PathVariable Integer id)
     {
         return new ResponseEntity(productService.update(product,id),HttpStatus.CREATED);
